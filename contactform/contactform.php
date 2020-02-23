b8a1c8afa213a62c614a9dc8b6a9f77036c8e96a
Daniel Hernandez <dnv19191@gmail.com>
2020-02-23 10:29:34 -0800

    Same old

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,28 +1,11 @@
 <?php
 
-if(isset($_POST)) {
-    $visitor_name = "";
-    $visitor_email = "";
-    $visitor_subject = "";
-    $visitor_message = "";
+if(isset($_POST['submit'])) {
+    $visitor_name = isset($_POST['visitor_name']);
+    $visitor_email = isset($_POST['visitor_name']);
+    $visitor_subject = isset($_POST['visitor_subject']);
+    $visitor_message = isset($_POST['visitor_message']);
     $recipient = "dnv19191@gmail.com";
-
-    if(isset($_POST['visitor_name'])) {
-        $visitor_name = filter_var($_POST['visitor_name'], FILTER_SANITIZE_STRING);
-    }
-
-    if(isset($_POST['visitor_email'])) {
-        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['visitor_email']);
-        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
-    }
-
-    if(isset($_POST['visitor_subject'])) {
-        $visitor_subject = filter_var($_POST['visitor_subject'], FILTER_SANITIZE_STRING);
-    }
-
-    if(isset($_POST['visitor_message'])) {
-        $visitor_message = htmlspecialchars($_POST['visitor_message']);
-    }
 
 
     $headers  = 'MIME-Version: 1.0' . "\r\n"
