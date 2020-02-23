3c4045d71f67b05c52bdc307a4e5ac9e9fdc06c3
Daniel Hernandez <dnv19191@gmail.com>
2020-02-23 10:23:52 -0800

    Same old

--- contactform/contactform.php
+++ contactform/contactform.php
@@ -1,6 +1,6 @@
 <?php
 
-
+if(isset($_POST)) {
     $visitor_name = "";
     $visitor_email = "";
     $visitor_subject = "";
@@ -29,9 +29,14 @@
     .'Content-type: text/html; charset=utf-8' . "\r\n"
     .'From: ' . $visitor_email . "\r\n";
 
-    mail($recipient, $visitor_subject, $visitor_message, $headers);
+    if(mail($recipient, $visitor_subject, $visitor_message, $headers)) {
+        echo "<p>Thank you for contacting me! , $visitor_name. Ill try to get back ASAP!.</p>";
+    } else {
+        echo '<p>We are sorry but the email did not go through.</p>';
+    }
 
-
-
+} else {
+    echo '<p>Something went wrong</p>';
+}
 
 ?>
